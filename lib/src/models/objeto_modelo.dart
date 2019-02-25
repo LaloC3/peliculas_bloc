a6f9cb49142ae60e8a4abba4d91a08adf130dbfa
Eduardo Contreras <lalo_grunge@hotmail.com>
2019-02-25 01:58:56 -0600

    nuevo2

--- lib/src/models/objeto_modelo.dart
+++ lib/src/models/objeto_modelo.dart
@@ -55,5 +55,41 @@ _directorio_poster =resultado['poster_path'];
 _lenguaje_original =resultado['original_language'];
 _titulo_original =resultado['original_title'];
 
+for (var i = 0; i < resultado['genre_ids'].length;  i++) {
+    _genero_id.add(resultado['genre_ids'][i]);
+}
+_fondo_directorio =resultado['backdrop_path'];
+_adulto =resultado['adult'];
+_fecha_realizada =resultado['release_date'];
+
  }
+
+String get fecha_realizada => _fecha_realizada;
+
+String get visto_general => _visto_general;
+
+bool get adulto => _adulto;
+
+String get fondo_directorio => _fondo_directorio; 
+
+List<int> get genero_ids => _genero_id;
+
+String get titulo_original => _titulo_original;
+
+String get lenguaje_original => _lenguaje_original;
+
+String get directorio_poster => _directorio_poster;
+
+double get popularidad => _popularidad;
+
+String get titulo => _titulo;
+
+String get voto_promedio => _voto_promedio;
+
+bool get video => _video;
+
+int get id => _id;
+
+int get voto_vontador => _voto_contador;
+
 }
